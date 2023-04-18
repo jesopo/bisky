71d099ff1a632d8f8fed7625055f559d6d835f9f
jesopo <github@lolnerd.net>
2023-04-18 09:10:10 +0000

    implement Labels on ProfileViewDetailed

--- src/lexicon/app/bsky/actor.rs
+++ src/lexicon/app/bsky/actor.rs
@@ -1,9 +1,22 @@
-use serde::Deserialize;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Label {
+    pub src: String,
+    pub uri: String,
+    pub val: String,
+    pub neg: bool,
+    pub cts: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ProfileViewDetailed {
     pub did: String,
     pub handle: String,
+    #[serde(rename(deserialize = "displayName"))]
+    pub display_name: Option<String>,
     #[serde(rename(deserialize = "postsCount"))]
     pub posts_count: Option<usize>,
+    pub labels: Vec<Label>,
 }
