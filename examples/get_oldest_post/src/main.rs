ed2977f75db1a7fa89f0db3d9e795d37a7f48485
jesopo <github@lolnerd.net>
2023-04-24 20:06:44 +0000

    make get_oldest_post cli args more understandable

--- examples/get_oldest_post/src/main.rs
+++ examples/get_oldest_post/src/main.rs
@@ -8,14 +8,21 @@ use url::Url;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Arguments {
+    /// A file to store JSON Web Tokens in
     #[clap(index = 1)]
     storage: PathBuf,
+    /// Which atproto service to connect to
     #[clap(index = 2)]
     service: Url,
+    /// Username to log in with
     #[clap(index = 3)]
     username: String,
+    /// Password to log in with
     #[clap(index = 4)]
     password: String,
+    /// Username to get oldest post for
+    #[clap(index = 5)]
+    query: String,
 }
 
 #[tokio::main]
@@ -30,7 +37,7 @@ async fn main() {
     }
 
     let mut client = Bluesky::new(Client::new(args.service, storage).await.unwrap());
-    let mut user = client.user(args.username);
+    let mut user = client.user(args.query);
     let posts = user.list_posts().await.unwrap();
     println!("oldest post: {:#?}", posts.last().unwrap());
 }
